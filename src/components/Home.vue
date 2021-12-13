9eea122ad934336bd3a2c3d5e6f98875bce17b27
vhho515 <89646443+vhho515@users.noreply.github.com>
2021-12-13 12:03:31 -0600

    Uploads
    Updated

---
+++ src/components/Home.vue
@@ -0,0 +1,97 @@
+ <template>
+  <v-main>
+    <v-container fluid>
+      <v-row>
+        <v-col align="center" justify="center" cols="12">
+          <blockquote>
+            <h1>Welcome {{validUserName}}! </h1>
+            <footer>
+              <h2>
+                <em>&mdash; Welcome to A+ Blog, come and express your creativity!</em>
+              </h2>
+            </footer>
+          </blockquote>
+        </v-col>
+      </v-row>
+    </v-container>
+
+    <v-container fluid>
+      <v-row
+        align="center"
+        justify="center"
+      >
+        <v-col
+          cols="12"
+            sm="10"
+            md="8"
+        >
+          <v-card class="elevation-2">
+            <v-container
+              fluid
+              grid-list-md
+            >
+              <v-row>
+                <v-col>
+                  <v-card>
+                    <v-img
+                      :src="require('@/assets/images/customer.jpg')" height="300px" width="380px">
+                    </v-img>
+                    <v-card-actions>
+                      <v-btn flat color="blue" @click="viewPosts">View Posts</v-btn>
+                    </v-card-actions>
+                  </v-card>
+                </v-col>
+                <v-col>
+                  <v-card>
+                    <v-img
+                      :src="require('@/assets/images/person.jpg')" height="300px" width="380px">
+                    </v-img>
+                    <v-card-actions>
+                      <v-btn flat color="blue" @click="viewgetPosts">Create a Profile</v-btn>
+                    </v-card-actions>
+                  </v-card>
+                </v-col>
+                <v-col>
+                  <v-card>
+                    <v-img
+                      :src="require('@/assets/images/edit.jpg')" height="300px" width="380px">
+                    </v-img>
+                    <v-card-actions>
+                      <v-btn flat color="blue" @click="viewgetPosts">View Profile</v-btn>
+                    </v-card-actions>
+                  </v-card>
+                </v-col>
+              </v-row>
+            </v-container>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-main>
+</template>
+
+<script>
+  import router from '../router';
+
+  export default {
+    name: 'Home',
+    data: () => ({
+      validUserName: "Guest"
+    }),
+    mounted() {
+      this.getUser();
+    },
+    methods: {
+      viewPosts() {
+        router.push('/post-list');
+      },
+      getUser() {
+        if (localStorage.getItem("isAuthenticates")
+          && JSON.parse(localStorage.getItem("isAuthenticates")) === true) {
+          this.validUserName = JSON.parse(localStorage.getItem("log_user"));
+        }
+      }
+    }
+  }
+</script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
